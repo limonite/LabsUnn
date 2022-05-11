a8380eaea534828b053cefd9c828081f196ac64a
limonite <92550714+limonite@users.noreply.github.com>
2022-05-11 16:24:21 +0300

    Add files via upload

---
+++ Операционные системы/лаб 2.cpp
@@ -0,0 +1,142 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+#include<locale.h>
+#include<time.h>
+
+int min(int* arr, int size);
+
+int main(void) {
+	setlocale(LC_ALL, "Russian");
+
+	int quantOfpr;
+	int check = 0;
+	int* id = NULL;
+	int* timeOfpr = NULL;
+	int quantumOfTime;
+	int remainTime=0;
+	int procTime = 0;
+	int allTime=0;
+	char choice;
+	int sumTime = 0;
+	int procces;
+	int iter = 0;
+	int* result = NULL;
+	int iR = 0;
+	int minpr = _CRT_INT_MAX;
+	FILE* f = fopen("input.txt", "r+");
+	FILE* in = fopen("output.txt", "w+");
+
+	printf_s("Выберете ввод данных:\n1. Ввести данные\n2. Считать данные с файла.\n3. Рандомизировать данные.\nВаш выбор: ");
+	scanf("%c", &choice);
+	printf_s("____________________\n");
+	switch (choice) {
+	case '1':
+		printf_s("Введите кол-во процессов: ");
+		scanf("%d", &quantOfpr);
+		id = (int*)malloc(sizeof(int) * quantOfpr);
+		timeOfpr = (int*)malloc(sizeof(int) * quantOfpr);
+		result = (int*)malloc(sizeof(int) * quantOfpr);
+		for (int i = 0; i < quantOfpr; i++) {
+			id[i] = i + 1;
+			printf_s("Процесс № %d: ", id[i]);
+			scanf("%d", &timeOfpr[i]);
+		}
+		printf_s("____________________\n");
+		break;
+	case '2':
+		fscanf(f, "%d", &quantOfpr);
+		id = (int*)malloc(sizeof(int) * quantOfpr);
+		timeOfpr = (int*)malloc(sizeof(int) * quantOfpr);
+		result = (int*)malloc(sizeof(int) * quantOfpr);
+		for (int i = 0; i < quantOfpr; i++) {
+			id[i] = i + 1;
+			fscanf(f, "%d", &timeOfpr[i]);
+		}
+		break;
+	case '3':
+		srand(time(0));
+		quantOfpr = 5 + rand() % 46;
+		id = (int*)malloc(sizeof(int) * quantOfpr);
+		timeOfpr = (int*)malloc(sizeof(int) * quantOfpr);
+		result = (int*)malloc(sizeof(int) * quantOfpr);
+		for (int i = 0; i < quantOfpr; i++) {
+			id[i] = i + 1;
+			timeOfpr[i] =1 + rand() % 50;
+		}
+		break;
+	default: exit(0);
+	}
+	fclose(f);
+
+
+	printf_s("Номер процесса: ");
+	for (int i = 0; i < quantOfpr; i++) {
+		printf_s("%-3d", id[i]);
+	}
+	printf_s("\n");
+	printf_s("Время процесса: ");
+	for (int i = 0; i < quantOfpr; i++) {
+		printf_s("%-3d", timeOfpr[i]);
+	}
+	printf_s("\n____________________\n");
+
+	while (true) {
+		iter++;
+		printf_s("________________________________________________________________________________\n");
+		if (quantOfpr > 20) fprintf(in,"________________________________________________________________________________\n");
+		printf_s("Введите квант времени: ");
+		scanf_s("%d", &quantumOfTime);
+		if ((quantumOfTime+remainTime) < min(timeOfpr, quantOfpr)) {
+			sumTime += quantumOfTime;
+			procTime += quantumOfTime;
+			remainTime = sumTime;
+			printf_s("Номер Итерации: %-3d Квант времени: %-4d  Время цп: %-4d Номер процессора: -- Время выбранного процесса: -- Оставшееся время %-4d|\n", iter, quantumOfTime, sumTime, remainTime);
+			if (quantOfpr>20) fprintf(in,"Номер Итерации: %-3d Квант времени: %-4d  Время цп: %-4d Номер процессора: -- Время выбранного процесса: -- Оставшееся время %-4d|\n", iter, quantumOfTime, sumTime, remainTime);
+			continue;
+		}
+		sumTime += quantumOfTime;
+		procTime += quantumOfTime;
+		while (sumTime >= min(timeOfpr, quantOfpr)) {
+			for (int i = 0; i < quantOfpr; i++) {
+				if ((minpr >= (sumTime - timeOfpr[i])) && (sumTime >= timeOfpr[i])) {
+					minpr = sumTime - timeOfpr[i];
+					procces = i;
+				}
+			}
+			remainTime = sumTime - timeOfpr[procces];
+			result[iR] = id[procces];
+			iR++;
+			if(quantOfpr<21) printf_s("Номер Итерации: %-3d Квант времени: %-4d  Время цп: %-4d Номер процесса: %-3d Время выбранного процесса: %-4d Оставшееся время %-4d|\n", iter, quantumOfTime, procTime, procces, timeOfpr[procces], remainTime);
+			else fprintf(in,"Номер Итерации: %-3d Квант времени: %-4d  Время цп: %-4d Номер процесса: %-3d Время выбранного процесса: %-4d Оставшееся время %-4d|\n", iter, quantumOfTime, procTime, procces, timeOfpr[procces], remainTime);
+			procTime -= timeOfpr[procces];
+			sumTime -= timeOfpr[procces];
+			timeOfpr[procces] = _CRT_INT_MAX;
+			minpr = _CRT_INT_MAX;
+		}
+		printf_s("________________________________________________________________________________\n");
+		if (quantOfpr > 20) fprintf(in,"________________________________________________________________________________\n");
+		check = 0;
+		for (int i = 0; i < quantOfpr; i++) {
+			if (timeOfpr[i] == _CRT_INT_MAX) {
+				check++;
+			}
+		}
+		if (check == quantOfpr) break;
+	}
+	printf_s("\n________________________________________________________________________________\n");
+	printf_s("Результат: ");
+	for (int i = 0; i < quantOfpr; i++) {
+		printf_s("%d   ", result[i]);
+		if(quantOfpr>20) fprintf(in,"%d   ", result[i]);
+	}
+	fclose(in);
+}
+
+int min(int* arr, int size) {
+	int min = arr[0];
+	for (int i = 1; i < size; i++) {
+		if (arr[i] < min) min = arr[i];
+	}
+	return min;
+}
---
+++ Операционные системы/лаб 3.cpp
@@ -0,0 +1,186 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+#include<locale.h>
+
+typedef struct queue {
+	int num;
+	queue* next;
+	queue* prev;
+}queue;
+
+queue* createQueue(queue* top, int n) {
+	queue* tmp = (queue*)malloc(sizeof(queue));
+	tmp->next = NULL;
+	tmp->prev = 0;
+	tmp->num = n;
+	return tmp;
+}
+
+queue* push(queue* top, int n) {
+	queue* tmpnew = (queue*)malloc(sizeof(queue));
+	queue* tmp = top;
+	tmpnew->prev = NULL;
+	tmpnew->num = n;
+	while (tmp->prev) tmp = tmp->prev;
+	tmpnew->next = tmp;
+	tmp->prev = tmpnew;
+	while (tmpnew->next) tmpnew=tmpnew->next;
+	return tmpnew;
+}
+
+int count(queue* top) {
+	if (!top) return 0;
+	queue* tmp = top;
+	int c = 0;
+	while (tmp->prev) {
+		c++;
+		tmp = tmp->prev;
+	}
+	return c+1;
+}
+
+int peek(queue* top) {
+	return top->num;
+}
+
+queue* pop(queue* top) {
+	queue* tmp = (queue*)malloc(sizeof(queue));
+	if (!top) {
+		printf_s("Очередь пуста!");
+		exit(0);
+	}
+	tmp = top->prev;
+	free(top->next);
+	top->next = NULL;
+	return tmp;
+}
+
+bool inQueue(queue* top, int n) {
+	queue* tmp = top;
+	while (tmp) {
+		if (tmp->num == n) return true;
+		tmp = tmp->prev;
+	}
+	return false;
+}
+
+int main(void) {
+	setlocale(LC_ALL, "Russian");
+
+	int** procceses;
+	char** result=NULL;
+	int pr;
+	float wait = 0, run = 0;
+	int min, minId;
+	int sum = 0;
+	int point=0;
+	int time = 0;
+	int quantOfprocceses;
+	char choice=NULL;
+	queue* top = NULL;
+
+	printf_s("Введите кол-во процессов: ");
+	scanf_s("%d", &quantOfprocceses);
+	procceses = (int**)malloc(sizeof(int*) * quantOfprocceses);
+	for (int i = 0; i < quantOfprocceses; i++) procceses[i] = (int*)malloc(sizeof(int) * 2);
+	for (int i = 0; i < quantOfprocceses; i++) {
+		printf_s("Время процесса №%d: ", i+1);
+		scanf_s("%d", &procceses[i][1]);
+		procceses[i][0] = i+1;
+		sum += procceses[i][1];
+	}
+	getchar();
+	while (true) {
+		printf_s("\n__________________________________________\n");
+		printf_s("Выберите алгоритм обработки:\n1. FCFS\n2. SJF\nВаш выбор: ");
+		scanf_s("%c", &choice);
+		getchar();
+		switch (choice) {
+		case '1':
+			result = (char**)malloc(sizeof(char*) * quantOfprocceses);
+			for (int i = 0; i < quantOfprocceses; i++) {
+				result[i] = (char*)malloc(sizeof(char) * sum);
+				for (int j = 0; j < sum; j++) result[i][j] = '\0';
+			}
+		    point = 0;
+			time = 0;
+			wait = 0, run = 0;
+			top = createQueue(top, procceses[0][0]);
+			for (int i = 1; i < quantOfprocceses; i++) {
+				top = push(top, procceses[i][0]);
+			}
+			while (top) {
+				pr = peek(top);
+				top = pop(top);
+				for (int j = 0; j < time; j++) {
+					result[pr - 1][j] = 'Г';
+					wait++;
+				}
+				for (int j = time; j < time + procceses[pr - 1][1]; j++) {
+					result[pr - 1][j] = 'И';
+					run++;
+				}
+				time += procceses[pr-1][1];
+			}
+			run /= quantOfprocceses;
+			wait /= quantOfprocceses;
+			printf_s("   1 ");
+			for (int i = 1; i < sum; i++) printf_s(" %-3d",i+1);
+			printf_s("\n");
+			for (int i = 0; i < quantOfprocceses; i++) {
+				printf_s("%d  ",i+1);
+				for (int j = 0; (result[i][j] != '\0')&&(j<sum); j++)printf_s("%c   ", result[i][j]);
+				printf_s("\n");
+			}
+			printf_s("\nОбщее время выполнения: %d\nСреднее время выполнения:%-3.2f\nСреднее время ожидания:%-3.2f", sum, run, wait);
+			break;
+		case '2':
+			result = (char**)malloc(sizeof(char*) * quantOfprocceses);
+			for (int i = 0; i < quantOfprocceses; i++) {
+				result[i] = (char*)malloc(sizeof(char) * sum);
+				for (int j = 0; j < sum; j++) result[i][j] = '\0';
+			}
+			point = 0;
+			time = 0;
+			wait = 0, run = 0;
+			while (count(top) != quantOfprocceses) {
+				min = _CRT_INT_MAX;
+				for (int i = 0; i < quantOfprocceses; i++) {
+					if ((procceses[i][1] <= min) && (!inQueue(top, procceses[i][0]))) {
+						minId = procceses[i][0];
+						min = procceses[i][1];
+					}
+				}
+				if (!top) top=createQueue(top, minId);
+				else top=push(top, minId);
+			}
+			while (top) {
+				pr = peek(top);
+				top = pop(top);
+				for (int j = 0; j < time; j++) {
+					result[pr - 1][j] = 'Г';
+					wait++;
+				}
+				for (int j = time; j < time + procceses[pr - 1][1]; j++) {
+					result[pr - 1][j] = 'И';
+					run++;
+				}
+				time += procceses[pr - 1][1];
+			}
+			run /= quantOfprocceses;
+			wait /= quantOfprocceses;
+			printf_s("   1 ");
+			for (int i = 1; i < sum; i++) printf_s(" %-3d", i + 1);
+			printf_s("\n");
+			for (int i = 0; i < quantOfprocceses; i++) {
+				printf_s("%d  ", i + 1);
+				for (int j = 0; (result[i][j] != '\0') && (j < sum); j++)printf_s("%c   ", result[i][j]);
+				printf_s("\n");
+			}
+			printf_s("\nОбщее время выполнения: %d\nСреднее время выполнения:%-3.2f\nСреднее время ожидания:%-3.2f", sum, run, wait);
+			break;
+		default: exit(0);
+		}
+	}
+}
